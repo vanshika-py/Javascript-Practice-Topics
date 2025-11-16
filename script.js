c870b3e2f723fd87b87f5854b71cdce4fabffa94
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2025-11-16 15:33:56 +0530

    first commit of Javascript topics practice

---
+++ script.js
@@ -0,0 +1,29 @@
+/*let employee = { 
+    name: "vanshika kathpal",
+    position:"full stack developer",
+    age : 21,
+    performance : function perf() {
+        console.log("vanshika is a very excellent full stack developer")
+    } 
+}
+employee.performance();*/
+
+let car = { 
+    name: "defender",
+    brand: "toyata",
+    price: "2 crore",
+    speed : 120,
+    color: "white",
+    status: function check() { 
+        if(this.speed > 100) { 
+            console.log("Buy this car")
+         } else { 
+                console.log("don't buy")
+    }
+}
+};
+
+car.status();
+
+
+let 
