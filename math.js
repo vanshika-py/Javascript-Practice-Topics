23cfd44ad8fab2c6cb04191ba9132578685e36a9
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-03-26 18:02:02 +0530

    exporting 3 function from module file

--- math.js
+++ math.js
@@ -1,3 +1,15 @@
-const object = require('./modules.js');
+// const object = require('./modules.js');
 
-console.log(object);
+// console.log(object);
+
+const {sum, subtract, divide} = require('./modules.js');
+
+let sumResult = sum(2,5);
+
+let subtractResult = subtract(10,20);
+
+let divideResult = divide(10,20);
+
+console.log(sumResult);
+console.log(subtractResult);
+console.log(divideResult);
--- modules.js
+++ modules.js
@@ -1,9 +1,28 @@
-let person  = {
-    Name : "vanshika",
-    Age : 21,
-    Department : "IT",
-    Salary : "40lpa",
-    Position : "Full Stack Developer"
+// let person  = {
+//     Name : "vanshika",
+//     Age : 21,
+//     Department : "IT",
+//     Salary : "40lpa",
+//     Position : "Full Stack Developer"
+// }
+
+// export default person.Age;
+
+
+function sum(a,b){
+    return a + b
 }
 
-export default person.Age;
+function subtract(b,a){
+    return a -b;
+}
+
+function divide(b,a){
+    return b / a;
+}
+
+module.exports{
+    sum,
+    subtract,
+    divide
+}
