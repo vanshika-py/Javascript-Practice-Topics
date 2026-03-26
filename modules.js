47f7c5815875bd3bacff7cf6753c58846048856d
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-03-26 17:29:13 +0530

    created an object for exporting it in math file

--- modules.js
+++ modules.js
@@ -1,5 +1,8 @@
-let name = "Vanshika Kathpal";
+let person  = {
+    Name : "vanshika";
+    Age : 21;
+    Department : "IT";
+    Salary : "40lpa";
+    Position : "Full Stack Developer"
+}
 
-
-
-module.exports = name;
