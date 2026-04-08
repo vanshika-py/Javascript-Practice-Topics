8d51ce80eb86b779e7172667ed289b2ed4441a06
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-04-08 12:41:04 +0530

    practiced proto

--- prototype.js
+++ prototype.js
@@ -3,14 +3,28 @@ const p1 = {
     fname : "vanshika",
     lname : "kathpal",
     getfullname (){
-        return `${fname} ${lname}`
-    }
+        return `${this.fname} ${this.lname}`;
+    },
 };
 
-console.log(p1);
+// console.log(p1);
 
-const p2 = {
-    fname : "diya",
-    lname : "kathpal"
-}
-console.log(p2.fname)
+// const p2 = {
+//     fname : "diya",
+//     lname : "kathpal"
+// }
+// console.log(p2.fname)
+
+//using prototype//
+
+ const p2 = Object.create (p1);
+
+// // console.log(p2);
+
+// console.log(p2.lname);
+
+console.log("p1 before is", p1.fname);
+
+p2.__proto__.fname = "Sumit";
+console.log("p1 after is", p1.fname);
+
